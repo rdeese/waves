207ba7eff033213ca87a70edbc5257e5eeb1586e
rdeese <rdeese@hmc.edu>
2018-01-10 20:11:28 -0600

    First attempt.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // (() => {
 //
 const chroma = require('chroma-js')
+const _ = require('lodash')
 
 class Pitch {
   static noteNumbers() {
@@ -21,8 +22,12 @@ class Pitch {
     }
   }
 
+  static visualFrequencyFromNumber(noteNumber) {
+    return /* 440 */ 0.6 * Math.pow(1.059463, noteNumber - 49)
+  }
+
   static frequencyFromNumber(noteNumber) {
-    return /* 440 */ 1 * Math.pow(1.059463, noteNumber - 49)
+    return 440 * Math.pow(1.0594630943592953, noteNumber - 49)
   }
 
   constructor(arg) {
@@ -38,6 +43,7 @@ class Pitch {
       this.note = Object.keys(Pitch.noteNumbers()).find(key => Pitch.noteNumbers()[key] === this.number)
     }
 
+    this.visualFrequency = Pitch.visualFrequencyFromNumber(this.number)
     this.frequency = Pitch.frequencyFromNumber(this.number)
     this.color = 'black' //chroma.hcl((this.number % 12) * 360/13, 80, 50).hex()
   }
@@ -68,6 +74,10 @@ class Canvas {
     this.context = this.canvas.getContext('2d')
   }
 
+  clear() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+  }
+
   center() {
     return new Point(this.canvas.width/2, this.canvas.height/2)
   }
@@ -78,10 +88,10 @@ class Canvas {
 
   drawPitch(pitch) {
     this.context.fillStyle = pitch.color
-    for (let i = 0; i < 100000; i++) {
+    for (let i = 0; i < 800000; i++) {
       let dot = this.randomPoint()
-      if (Math.sin(dot.distanceFrom(this.center())*pitch.frequency) > Random.inRange(-1, 1)) {
-        this.context.fillRect(dot.x, dot.y, 0.4, 0.4)
+      if (Math.sin(dot.distanceFrom(this.center())*pitch.visualFrequency) > Random.inRange(-1, 1)) {
+        this.context.fillRect(dot.x, dot.y, 0.8, 0.8)
       }
     }
   }
@@ -109,15 +119,72 @@ const appendCanvas = (noteNames, width, height) => {
 }
 
 const main = () => {
-  const canvasList = Object.keys(Pitch.noteNumbers()).map((note) => { return ['A', note] })
-  
-  const limitingDimension = Math.min(window.innerWidth, window.innerHeight)/2
+  const limitingDimension = Math.min(window.innerWidth, window.innerHeight)
   const width = limitingDimension - limitingDimension / 5
   const height = width
 
-  for (let i in canvasList) {
-    appendCanvas(canvasList[i], width, height)
+  const canvasElement = document.getElementById('canvas')
+  canvasElement.width = width
+  canvasElement.height = height
+  const canvas = new Canvas(canvasElement)
+
+  const activePitches = []
+  const activeOscillators = {
   }
+
+  const keyMap = {
+    'a': new Pitch('A'),
+    'w': new Pitch('A#'),
+    's': new Pitch('B'),
+    'd': new Pitch('C'),
+    'r': new Pitch('C#'),
+    'f': new Pitch('D'),
+    't': new Pitch('D#'),
+    'g': new Pitch('E'),
+    'h': new Pitch('F'),
+    'u': new Pitch('F#'),
+    'j': new Pitch('G'),
+    'i': new Pitch('G#'),
+    'k': new Pitch("A'")
+  }
+
+  const audioContext = new AudioContext()
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key in keyMap && !_.includes(activePitches, keyMap[event.key])) {
+      const newPitch = keyMap[event.key]
+      canvas.clear()
+      activePitches.push(newPitch)
+      for (let i in activePitches) {
+        canvas.drawPitch(activePitches[i])
+      }
+
+      let oscillator = activeOscillators[newPitch.note]
+      if (!oscillator) {
+        oscillator = audioContext.createOscillator()
+        oscillator.type = 'triangle'
+        activeOscillators[newPitch.note] = oscillator
+        oscillator.frequency.value = newPitch.frequency
+        oscillator.start()
+      }
+      oscillator.connect(audioContext.destination)
+    }
+  })
+
+  document.addEventListener('keyup', (event) => {
+    if (event.key in keyMap) {
+      const targetPitch = keyMap[event.key]
+
+      canvas.clear()
+      _.pull(activePitches, targetPitch)
+      for (let i in activePitches) {
+        canvas.drawPitch(activePitches[i])
+      }
+     
+      const oscillator = activeOscillators[targetPitch.note]
+      oscillator.disconnect()
+    }
+  })
 }
 
 window.onload = main
